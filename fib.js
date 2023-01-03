c9f80b6d11024c55457e5acbe0d33adbe61c2c31
Khanh Le <91043526+ibretsam@users.noreply.github.com>
2023-01-03 20:14:34 +0700

    enhance fib with memoization

--- fib.js
+++ fib.js
@@ -1,6 +1,8 @@
-const fib = (n) => {
+const fib = (n, memo = {}) => {
+  if (n in memo) return memo[n];
   if (n <= 2) return 1;
-  return fib(n - 1) + fib(n - 2);
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
 };
 
-console.log(fib(50));
+console.log(fib(1476));
