9b7bd3728695570205530327709804cce9f55b2d
Khanh Le <91043526+ibretsam@users.noreply.github.com>
2023-01-04 20:36:48 +0700

    Added memoization on canSum

--- canSum.js
+++ canSum.js
@@ -1,14 +1,17 @@
-const canSum = (target, number) => {
+const canSum = (target, number, memo = {}) => {
+  if (target in memo) return memo[target];
   if (target === 0) return true;
   if (target < 0) return false;
 
   for (let num of number) {
-    let remainder = target - num;
-    if (canSum(remainder, number) === true) {
+    const remainder = target - num;
+    if (canSum(remainder, number, memo) === true) {
+      memo[target] = true;
       return true;
     }
   }
 
+  memo[target] = false;
   return false;
 };
 
@@ -16,4 +19,4 @@ console.log(canSum(7, [2, 3]));
 console.log(canSum(7, [5, 3, 4, 7]));
 console.log(canSum(7, [2, 4]));
 console.log(canSum(8, [2, 3, 5]));
-console.log(canSum(300, [7, 14]));
+console.log(canSum(30000, [7, 14, 1]));
