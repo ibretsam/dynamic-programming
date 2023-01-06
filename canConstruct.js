34eba0b261751e8786630add1e05ea100b3be4c5
Khanh Le <91043526+ibretsam@users.noreply.github.com>
2023-01-06 20:59:28 +0700

    Added canConstruct memoized version

--- canConstruct.js
+++ canConstruct.js
@@ -1,14 +1,18 @@
-const canConstruct = (target, wordbank) => {
+const canConstruct = (target, wordbank, memo = {}) => {
   if (target === "") return true;
+  if (target in memo) return memo[target];
+
   for (let word of wordbank) {
     if (target.indexOf(word) === 0) {
       const remainWord = target.replace(word, "");
-      if (canConstruct(remainWord, wordbank) === true) {
+      if (canConstruct(remainWord, wordbank, memo) === true) {
+        memo[target] = true;
         return true;
       }
     }
   }
 
+  memo[target] = false;
   return false;
 };
 
