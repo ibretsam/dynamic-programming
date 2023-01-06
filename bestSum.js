39ca3e0eab73f108627eb81633f44734731b9ce6
Khanh Le <91043526+ibretsam@users.noreply.github.com>
2023-01-06 20:25:16 +0700

    Added bestSum memoized version

--- bestSum.js
+++ bestSum.js
@@ -1,12 +1,13 @@
-const bestSum = (target, numbers) => {
+const bestSum = (target, numbers, memo = {}) => {
   if (target === 0) return [];
   if (target < 0) return null;
+  if (target in memo) return memo[target];
 
   let bestCombination = null;
 
   for (let num of numbers) {
     const remainder = target - num;
-    const remainderCombination = bestSum(remainder, numbers);
+    const remainderCombination = bestSum(remainder, numbers, memo);
     if (remainderCombination !== null) {
       const combination = [...remainderCombination, num];
       if (
@@ -17,11 +18,11 @@ const bestSum = (target, numbers) => {
       }
     }
   }
-
+  memo[target] = bestCombination;
   return bestCombination;
 };
 
 console.log(bestSum(7, [5, 3, 4, 7]));
 console.log(bestSum(8, [2, 3, 5]));
 console.log(bestSum(8, [1, 4, 5]));
-console.log(bestSum(100, [1, 2, 5, 25]));
+console.log(bestSum(1000, [1, 2, 5, 25]));
