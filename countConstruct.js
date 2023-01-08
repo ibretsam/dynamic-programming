8258ac94d89be944684249e00d42f4f84dad3519
Khanh Le <masterbruculous@gmail.com>
2023-01-08 21:58:17 +0700

    added countConstruct memoized

--- countConstruct.js
+++ countConstruct.js
@@ -1,16 +1,18 @@
-const countConstruct = (target, wordbank) => {
+const countConstruct = (target, wordbank, memo = {}) => {
   if (target === "") return 1;
+  if (target in memo) return memo[target];
 
   let count = 0;
 
   for (let word of wordbank) {
     if (target.indexOf(word) === 0) {
       const remainWord = target.replace(word, "");
-      const numWaysForRest = countConstruct(remainWord, wordbank);
+      const numWaysForRest = countConstruct(remainWord, wordbank, memo);
       count += numWaysForRest;
     }
   }
 
+  memo[target] = count;
   return count;
 };
 
